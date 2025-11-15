13e16bd5039e272b7e03f00b6de8bace99637588
Usman-bin-Khalid <usmanbinkhalidpk@gmail.com>
2025-11-15 20:14:33 +0500

    Reusable Widgets for Text Completd

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:pharmacy_app/widgets/support_widget.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool allMedicine = true, suppliment = false, herbal = false, vitamins = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,12 +33,12 @@ class Home extends StatelessWidget {
             Text('Online Pharmacy', style: AppWidget.lightTextStyle(28.0)),
             SizedBox(height: 30.0),
             Padding(
-              padding: EdgeInsets.only(left: 30.0),
+              padding: EdgeInsets.only(left: 0.0),
               child: Row(
                 children: [
                   Expanded(
                     child: Container(
-                      margin: EdgeInsets.only(right: 40),
+                      margin: EdgeInsets.only(right: 17),
                       decoration: BoxDecoration(
                         color: Color.fromARGB(255, 225, 219, 248),
                         borderRadius: BorderRadius.circular(30.0),
@@ -40,9 +46,11 @@ class Home extends StatelessWidget {
                       ),
                       child: Center(
                         child: TextField(
+                               textAlignVertical: TextAlignVertical.center, // text ko center mai lany ky liy
                           decoration: InputDecoration(
+                            
                             hintText: 'Search Medicine...',
-                            hintStyle: AppWidget.lightTextStyle(18.0),
+                            hintStyle: AppWidget.lightTextStyle(15.0),
                             border: InputBorder.none,
                             suffixIcon: Container(
                               margin: EdgeInsets.all(3),
@@ -56,33 +64,108 @@ class Home extends StatelessWidget {
                             ),
                           ),
                         ),
+                        
                       ),
                     ),
                   ),
                 ],
               ),
             ),
-            SizedBox(height: 50.0),
+      
+      
+            SizedBox(height: 30.0),
             Container(
               height: 50,
 
               child: ListView(
                 scrollDirection: Axis.horizontal,
                 children: [
-                  Container(
-                    height: 50,
-                    decoration: BoxDecoration(color: Colors.black),
-                    child: Center(
-                      child: Text(
-                        'All Medicines',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'Poppins',
-                          fontSize: 20.0,
+                  allMedicine
+                      ? AppWidget.selectedCategory('All Medicines')
+                      : GestureDetector(
+                          onTap: () {
+                            allMedicine = true;
+                            suppliment = false;
+                            vitamins = false;
+                            herbal = false;
+                            setState(() {});
+                          },
+                          child: Container(
+                            height: 50,
+                            child: Center(
+                              child: Text(
+                                'All Medicines',
+                                style: AppWidget.lightTextStyle(14),
+                              ),
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
-                  ),
+                  SizedBox(width: 12),
+                  suppliment
+                      ? AppWidget.selectedCategory('Suppliment')
+                      : GestureDetector(
+                          onTap: () {
+                            allMedicine = false;
+                            suppliment = true;
+                            vitamins = false;
+                            herbal = false;
+                            setState(() {});
+                          },
+                          child: Container(
+                            height: 50,
+                            child: Center(
+                              child: Text(
+                                'Suppliment',
+                                style: AppWidget.lightTextStyle(14),
+                              ),
+                            ),
+                          ),
+                        ),
+                  SizedBox(width: 12),
+                  vitamins
+                      ? AppWidget.selectedCategory('Vitamins')
+                      : GestureDetector(
+                          onTap: () {
+                            allMedicine = false;
+                            vitamins = true;
+                            suppliment = false;
+                            herbal = false;
+                            setState(() {});
+                          },
+                          child: Container(
+                               height: 50,
+                            child: Center(
+                              child: Text(
+                                'Vitamins',
+                                style: AppWidget.lightTextStyle(14),
+                              ),
+                            ),
+                          ),
+                        ),
+               SizedBox(width: 12),
+                  herbal
+                      ? AppWidget.selectedCategory('Herbal')
+                      : GestureDetector(
+                          onTap: () {
+                            allMedicine = false;
+                            vitamins = false;
+                            suppliment = false;
+                            herbal = true;
+                            setState(() {});
+                          },
+                          child: Container(
+                               height: 50,
+                            child: Center(
+                              child: Text(
+                                'Herbal',
+                                style: AppWidget.lightTextStyle(14),
+                              ),
+                            ),
+                          ),
+                        ),
+              
+              
+              
                 ],
               ),
             ),
--- lib/widgets/support_widget.dart
+++ lib/widgets/support_widget.dart
@@ -2,9 +2,40 @@ import 'package:flutter/material.dart';
 
 class AppWidget {
   static TextStyle headlineTextStyle(double size) {
-    return TextStyle(fontFamily: 'Poppins', color: Colors.black, fontSize: size);
+    return TextStyle(
+      fontFamily: 'Poppins',
+      color: Colors.black,
+      fontSize: size,
+    );
   }
+
   static TextStyle lightTextStyle(double size) {
-    return TextStyle(fontFamily: 'Poppins1' , color: Colors.black, fontSize: 28);
+    return TextStyle(fontFamily: 'Poppins1', color: Colors.black, fontSize: size);
+  }
+
+  static Widget selectedCategory(String name) {
+    return Material(
+      elevation: 3.0,
+      borderRadius: BorderRadius.circular(30),
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        height: 50,
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(30),
+        ),
+
+        child: Center(
+          child: Text(
+            'All Medicines',
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'Poppins',
+              fontSize: 16.0,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
