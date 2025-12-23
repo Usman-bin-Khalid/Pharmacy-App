02a42e77660be1c9eaa0c8385254dcae8e31d208
Usman-bin-Khalid <usmanbinkhalidpk@gmail.com>
2025-12-23 13:02:30 +0500

    Get All Products function of Firebase Created

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,16 +1,24 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/widgets.dart';
 
 class DatabaseMethods {
   Future addUserInfo(Map<String, dynamic> userInfoMap, String id) {
-      return FirebaseFirestore.instance
-          .collection("pharmacy_users")
-          .doc(id)
-          .set(userInfoMap);
+    return FirebaseFirestore.instance
+        .collection("pharmacy_users")
+        .doc(id)
+        .set(userInfoMap);
   }
+
   Future addProduct(Map<String, dynamic> productInfoMap) {
-      return FirebaseFirestore.instance
-          .collection("Products")
-          
-          .add(productInfoMap);
+    return FirebaseFirestore.instance
+        .collection("Products")
+        .add(productInfoMap);
+  }
+
+  Future<Stream<QuerySnapshot>> getAllProducts(String category) async {
+    return await FirebaseFirestore.instance
+        .collection('Products')
+        .where('Category', isEqualTo: category)
+        .snapshots();
   }
 }
