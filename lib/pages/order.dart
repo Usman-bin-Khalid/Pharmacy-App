281cbd47bc36bad25c0d210dad5d6f71108071c0
Usman-bin-Khalid <usmanbinkhalidpk@gmail.com>
2025-12-23 21:57:55 +0500

    Order Page UI Completed

--- lib/pages/order.dart
+++ lib/pages/order.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pharmacy_app/widgets/support_widget.dart';
 class OrderPage extends StatefulWidget {
   const OrderPage({super.key});
 
@@ -9,6 +10,61 @@ class OrderPage extends StatefulWidget {
 class _OrderPageState extends State<OrderPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+  backgroundColor: Color(0xffd1cfeb),
+      body: Container(
+        margin: EdgeInsets.only(top: 60, left: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Order Page' , style: AppWidget.headlineTextStyle(25),),
+            SizedBox(height: 20,),
+            Container(
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 225, 219, 248),
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(right: 30),
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      
+                      gradient: LinearGradient(
+                          colors: [
+                            Color(0xffbab3a6),
+                            Color(0xffddd7cd),
+                            Color(0xffa59c8f),
+                          ],
+                      ),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: Image.asset('assets/images/medicine.png',
+                    height: 120,
+                    width: 120,
+                  fit: BoxFit.fill,
+                    ),
+                  ),
+                  SizedBox(width: 20,),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Paracitamol' , style: AppWidget.headlineTextStyle(18),),
+                      SizedBox(height: 2,),
+                      Text("Quantity : " + "5", style: AppWidget.headlineTextStyle(16),),
+                        SizedBox(height: 2,),
+                      Text("Google" , style: AppWidget.headlineTextStyle(15),),
+                        SizedBox(height: 2,),
+                      Text("Total Price : " + "\$20.0", style: AppWidget.headlineTextStyle(15),)
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
