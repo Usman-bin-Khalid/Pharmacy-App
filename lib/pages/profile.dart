fcdc23e17f17bad79c1bf787b7ef02df9c908afc
Usman-bin-Khalid <usmanbinkhalidpk@gmail.com>
2026-05-10 15:42:20 +0500

    Project Completed

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -52,185 +52,209 @@ class _ProfileState extends State<Profile> {
       ),
     );
     return Scaffold(
-            extendBodyBehindAppBar: true,
+           
       backgroundColor: const Color(0xffd1cfeb),
       body: name == null
           ? const Center(child: CircularProgressIndicator())
-          : SingleChildScrollView(
-              child: Column(
-                children: [
-                  Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Container(
-                        height: MediaQuery.of(context).size.height / 3.0,
-                        width: MediaQuery.of(context).size.width,
-                        decoration: const BoxDecoration(
-                          gradient: LinearGradient(
-                            colors: [Color(0xff6a5ae0), Color(0xff9087e5)],
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight,
+          : MediaQuery.removePadding(
+              context: context,
+        removeTop: true,
+            child: SingleChildScrollView(
+              padding: EdgeInsets.zero,
+                child: Column(
+                  children: [
+                    Container(
+                      width: double.infinity,
+                          decoration: const BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: [Color(0xff6a5ae0), Color(0xff9087e5)],
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight,
+                              ),
+                              borderRadius: BorderRadius.vertical(
+                                bottom: Radius.circular(50),
+                              ),
+                            ),
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          Container(
+                            height: MediaQuery.of(context).size.height / 3.0,
+                            width: MediaQuery.of(context).size.width,
+                            decoration: const BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: [Color(0xff6a5ae0), Color(0xff9087e5)],
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight,
+                              ),
+                              borderRadius: BorderRadius.vertical(
+                                bottom: Radius.circular(50),
+                              ),
+                            ),
                           ),
-                          borderRadius: BorderRadius.vertical(
-                            bottom: Radius.circular(50),
+                         
+                         
+                         
+                          Positioned(
+                            top: 50,
+                            right: 20,
+                            child: GestureDetector(
+                              onTap: () async {
+                                bool? updated = await Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => EditProfile(
+                                      id: id!,
+                                      name: name!,
+                                      phone: phone!,
+                                      address: address!,
+                                      allergies: allergies!,
+                                      image: image ?? "",
+                                    ),
+                                  ),
+                                );
+                                if (updated == true) {
+                                  getthesharedpref();
+                                }
+                              },
+                              child: Container(
+                                padding: const EdgeInsets.all(8),
+                                decoration: BoxDecoration(
+                                  color: Colors.white.withOpacity(0.2),
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: const Icon(Icons.edit, color: Colors.white),
+                              ),
+                            ),
                           ),
-                        ),
-                      ),
-                      Positioned(
-                        top: 50,
-                        right: 20,
-                        child: GestureDetector(
-                          onTap: () async {
-                            bool? updated = await Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => EditProfile(
-                                  id: id!,
-                                  name: name!,
-                                  phone: phone!,
-                                  address: address!,
-                                  allergies: allergies!,
-                                  image: image ?? "",
+                          Column(
+                            children: [
+                              const SizedBox(height: 70),
+                              Container(
+                                padding: const EdgeInsets.all(5),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  shape: BoxShape.circle,
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.black.withOpacity(0.1),
+                                      blurRadius: 20,
+                                      offset: const Offset(0, 10),
+                                    ),
+                                  ],
+                                ),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(70),
+                                  child: image == null || image == ""
+                                      ? Image.asset(
+                                          "assets/images/profile.png",
+                                          height: 130,
+                                          width: 130,
+                                          fit: BoxFit.fill,
+                                        )
+                                      : Image.network(
+                                          image!,
+                                          height: 130,
+                                          width: 130,
+                                          fit: BoxFit.fill,
+                                        ),
                                 ),
                               ),
-                            );
-                            if (updated == true) {
-                              getthesharedpref();
-                            }
-                          },
-                          child: Container(
-                            padding: const EdgeInsets.all(8),
-                            decoration: BoxDecoration(
-                              color: Colors.white.withOpacity(0.2),
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            child: const Icon(Icons.edit, color: Colors.white),
-                          ),
-                        ),
-                      ),
-                      Column(
-                        children: [
-                          const SizedBox(height: 70),
-                          Container(
-                            padding: const EdgeInsets.all(5),
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              shape: BoxShape.circle,
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.black.withOpacity(0.1),
-                                  blurRadius: 20,
-                                  offset: const Offset(0, 10),
+                              const SizedBox(height: 15),
+                              Text(
+                                name!,
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 26.0,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: 'FredokaBold',
                                 ),
-                              ],
-                            ),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(70),
-                              child: image == null || image == ""
-                                  ? Image.asset(
-                                      "assets/images/profile.png",
-                                      height: 130,
-                                      width: 130,
-                                      fit: BoxFit.fill,
-                                    )
-                                  : Image.network(
-                                      image!,
-                                      height: 130,
-                                      width: 130,
-                                      fit: BoxFit.fill,
-                                    ),
-                            ),
+                              ),
+                              Text(
+                                email!,
+                                style: TextStyle(
+                                  color: Colors.white.withOpacity(0.8),
+                                  fontSize: 16.0,
+                                  fontFamily: 'Poppins',
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                            ],
                           ),
-                          const SizedBox(height: 15),
-                          Text(
-                            name!,
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 26.0,
-                              fontWeight: FontWeight.bold,
-                              fontFamily: 'FredokaBold',
-                            ),
-                          ),
-                          Text(
-                            email!,
-                            style: TextStyle(
-                              color: Colors.white.withOpacity(0.8),
-                              fontSize: 16.0,
-                              fontFamily: 'Poppins',
-                            ),
-                          ),
-                          SizedBox(height: 10),
                         ],
                       ),
-                    ],
-                  ),
-                  const SizedBox(height: 30),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: Column(
-                      children: [
-                        _buildProfileCard(Icons.person, "Full Name", name!),
-                        _buildProfileCard(
-                          Icons.calendar_month,
-                          "Date of Birth",
-                          dob!,
-                        ),
-                        _buildProfileCard(Icons.wc, "Gender", gender!),
-                        _buildProfileCard(Icons.phone, "Phone Number", phone!),
-                        _buildProfileCard(
-                          Icons.location_on,
-                          "Primary Address",
-                          address!,
-                        ),
-                        _buildProfileCard(
-                          Icons.medical_services,
-                          "Allergies & Notes",
-                          allergies!,
-                        ),
-                        const SizedBox(height: 30),
-                        GestureDetector(
-                          onTap: () {
-                            Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const Login(),
-                              ),
-                            );
-                          },
-                          child: Container(
-                            padding: const EdgeInsets.symmetric(vertical: 18),
-                            width: double.infinity,
-                            decoration: BoxDecoration(
-                              color: Colors.red.withOpacity(0.1),
-                              borderRadius: BorderRadius.circular(18),
-                              border: Border.all(
-                                color: Colors.red.withOpacity(0.3),
-                              ),
-                            ),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: const [
-                                Icon(Icons.logout, color: Colors.red),
-                                SizedBox(width: 10),
-                                Text(
-                                  "Logout from Account",
-                                  style: TextStyle(
-                                    color: Colors.red,
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                  ),
+                    ),
+                
+                
+                
+                    const SizedBox(height: 30),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: Column(
+                        children: [
+                          _buildProfileCard(Icons.person, "Full Name", name!),
+                          _buildProfileCard(
+                            Icons.calendar_month,
+                            "Date of Birth",
+                            dob!,
+                          ),
+                          _buildProfileCard(Icons.wc, "Gender", gender!),
+                          _buildProfileCard(Icons.phone, "Phone Number", phone!),
+                          _buildProfileCard(
+                            Icons.location_on,
+                            "Primary Address",
+                            address!,
+                          ),
+                          _buildProfileCard(
+                            Icons.medical_services,
+                            "Allergies & Notes",
+                            allergies!,
+                          ),
+                          const SizedBox(height: 30),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const Login(),
                                 ),
-                              ],
+                              );
+                            },
+                            child: Container(
+                              padding: const EdgeInsets.symmetric(vertical: 18),
+                              width: double.infinity,
+                              decoration: BoxDecoration(
+                                color: Colors.red.withOpacity(0.1),
+                                borderRadius: BorderRadius.circular(18),
+                                border: Border.all(
+                                  color: Colors.red.withOpacity(0.3),
+                                ),
+                              ),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: const [
+                                  Icon(Icons.logout, color: Colors.red),
+                                  SizedBox(width: 10),
+                                  Text(
+                                    "Logout from Account",
+                                    style: TextStyle(
+                                      color: Colors.red,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
-                        ),
-                        const SizedBox(height: 50),
-                      ],
+                          const SizedBox(height: 50),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
+          ),
     );
   }
 
